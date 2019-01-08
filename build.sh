db244dc83df2afd03f45e533e21998dcdb835ad2
Kacper Mentel <mentel.kk@gmail.com>
2019-01-08 17:07:40 +0100

    Add script for docker image build automation

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+# Example of usage:
+# IMAGE=rabbitmq VERSION=3.7.9 ERLANG_VERSION=21.1 ELIXIR_VERSION=1.7.4 CONTEXT_PATH=../rabbitmq-server ./build.sh
+
+docker build -t ${IMAGE}:${VERSION} \
+       --build-arg erlang_version=${ERLANG_VERSION} \
+       --build-arg elixir_version=${ELIXIR_VERSION} \
+       --build-arg server_release_version=${VERSION} \
+       -f Dockerfile \
+       $CONTEXT_PATH
